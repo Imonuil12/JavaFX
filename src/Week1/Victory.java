d99385f39e53683f83d42203cc1eb0680b5db3de
ImonuilSuleimanov <imonuil.suleimanov_2025@ucentralasia.org>
2022-09-20 14:25:59 +0600

    Problem sets are committed for the first time

---
+++ src/Week1/Victory.java
@@ -0,0 +1,9 @@
+package Week1;
+
+public class Victory {
+    public static void main(String[] args) {
+        System.out.println("//////////////////////\n" +
+                "|| Week1.Victory is mine! ||\n" +
+                "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
