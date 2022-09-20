ec3de3d6a6fd0885b3f5817798f9b4d8d6e2cc5f
ImonuilSuleimanov <imonuil.suleimanov_2025@ucentralasia.org>
2022-09-21 00:19:27 +0600

    Few other problems are solved and added

--- src/Week2/FightSong.java
+++ src/Week2/FightSong.java
@@ -1,5 +1,28 @@
 package Week2;
 
 public class FightSong {
-
+    public static void main(String[] args) {
+        printShortChorus();
+        printLongChorus();
+        printLongChorus();
+        printShortChorus();
+    }
+    public static void printFirstCouplet() {
+        System.out.println("Go, team, go!\n" +
+                "You can do it.");
+    }
+    public static void printSecondCouplet() {
+        System.out.println("You're the best,\n" +
+                "In the West.");
+    }
+    public static void printShortChorus() {
+        printFirstCouplet();
+        System.out.println();
+    }
+    public static void printLongChorus() {
+        printFirstCouplet();
+        printSecondCouplet();
+        printFirstCouplet();
+        System.out.println();
+    }
 }
