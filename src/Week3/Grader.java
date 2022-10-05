210fa03e1ff1b04b6e85cec0e526f29754eb6a89
ImonuilSuleimanov <imonuil.suleimanov_2025@ucentralasia.org>
2022-10-05 09:12:14 +0600

    Few other problems solved and added

--- src/Week3/Grader.java
+++ src/Week3/Grader.java
@@ -3,16 +3,8 @@ import java.util.Scanner;
 public class Grader {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print(">>> ");
-        int grade = input.nextInt();
-        if (grade >= 50 && grade <= 100) {
-            System.out.println("Passed");
-        }
-        else if (grade >= 0 && grade < 50) {
-            System.out.println("Failed");
-        }
-        else {
-            System.out.println("Incorrect grade!");
-        }
+        System.out.println(">>>");
+
+        int grader = input.nextInt();
     }
 }
---
+++ src/Week4/ArraysAndArraylist.java
@@ -0,0 +1,5 @@
+package Week4;
+
+public class ArraysAndArraylist {
+    int [] n = {10,20,30,40,50,1,3,4,5,6};
+}
